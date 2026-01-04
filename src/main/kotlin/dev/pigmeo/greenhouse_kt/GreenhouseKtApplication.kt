f74300e470bfdf137c7cfbf2000789c71bd6b2dc
Victor Pigmeo <victor.blq@gmail.com>
2026-01-04 15:19:30 -0300

    First commit

---
+++ src/main/kotlin/dev/pigmeo/greenhouse_kt/GreenhouseKtApplication.kt
@@ -0,0 +1,11 @@
+package dev.pigmeo.greenhouse_kt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GreenhouseKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<GreenhouseKtApplication>(*args)
+}
