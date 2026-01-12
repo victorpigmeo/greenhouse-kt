63e363cffd24afd3bb07929565ae9331bf2bedab
Victor Pigmeo <victor.blq@gmail.com>
2026-01-12 18:45:55 -0300

    Now i'm feeling it

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-	kotlin("jvm") version "2.3.0"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.5.9"
-	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "1.9.25"
-	kotlin("kapt") version "2.0.21"
-	id("org.graalvm.buildtools.native") version "0.10.2"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.9"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("kapt") version "2.0.21"
+    id("org.graalvm.buildtools.native") version "0.11.3"
 }
 
 group = "dev.pigmeo"
@@ -13,20 +13,20 @@ version = "0.0.1-SNAPSHOT"
 description = "Greenhouse control"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2025.0.1")
-		mavenBom("io.github.resilience4j:resilience4j-bom:2.3.0")
-	}
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2025.0.1")
+        mavenBom("io.github.resilience4j:resilience4j-bom:2.3.0")
+    }
 }
 
 val mapstructVersion = "1.6.3"
@@ -34,63 +34,66 @@ val resilience4jVersion = "2.3.0"
 val loggerVersion = "7.0.3"
 
 dependencies {
-	// Spring
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-aop")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
 
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-	// Resilience4j
-	implementation("io.github.resilience4j:resilience4j-spring-boot3:${resilience4jVersion}")
+    // Resilience4j
+    implementation("io.github.resilience4j:resilience4j-spring-boot3:${resilience4jVersion}")
 
-	// Mapstruct
-	implementation("org.mapstruct:mapstruct:$mapstructVersion")
-	kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
+    // Mapstruct
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    kapt("org.mapstruct:mapstruct-processor:$mapstructVersion")
 
-	// Postgres
-	runtimeOnly("org.postgresql:postgresql")
+    // Postgres
+    runtimeOnly("org.postgresql:postgresql")
 
-	//Logger
-	implementation("io.github.oshai:kotlin-logging-jvm:$loggerVersion")
+    //Logger
+    implementation("io.github.oshai:kotlin-logging-jvm:$loggerVersion")
 
-	// Other
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // Other
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	// Tests
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Tests
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	jvmToolchain(21)
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    jvmToolchain(21)
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 allOpen {
-	annotation("jakarta.persistence.Entity")
-	annotation("jakarta.persistence.MappedSuperclass")
-	annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 graalvmNative {
-	binaries.all {
-		buildArgs.add("--gc=epsilon")
-	}
+    binaries {
+        named("main") {
+            imageName.set("greenhouse-kt")
+            buildArgs.add("--no-fallback")
+        }
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
