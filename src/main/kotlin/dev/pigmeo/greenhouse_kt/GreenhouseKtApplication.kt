c7d2d24dc7b78145a7c5ed8c9f2ea457faa3f69e
Victor Pigmeo <victor.blq@gmail.com>
2026-01-13 10:34:42 -0300

    Fixing jackson reflection

--- src/main/kotlin/dev/pigmeo/greenhouse_kt/GreenhouseKtApplication.kt
+++ src/main/kotlin/dev/pigmeo/greenhouse_kt/GreenhouseKtApplication.kt
@@ -1,9 +1,26 @@
 package dev.pigmeo.greenhouse_kt
 
+import dev.pigmeo.greenhouse_kt.application.payloads.DhtReadDtoOut
+import dev.pigmeo.greenhouse_kt.application.payloads.DhtReadEspDtoIn
+import dev.pigmeo.greenhouse_kt.application.payloads.GpioDtoIn
+import dev.pigmeo.greenhouse_kt.application.payloads.GpioDtoOut
+import dev.pigmeo.greenhouse_kt.application.payloads.GpioEspDtoIn
+import dev.pigmeo.greenhouse_kt.application.payloads.GpioEspDtoOut
+import dev.pigmeo.greenhouse_kt.application.payloads.LoginRequest
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@RegisterReflectionForBinding(
+	LoginRequest::class,
+	GpioDtoOut::class,
+	GpioDtoIn::class,
+	GpioEspDtoOut::class,
+	GpioEspDtoIn::class,
+	DhtReadDtoOut::class,
+	DhtReadEspDtoIn::class
+)
 class GreenhouseKtApplication
 
 fun main(args: Array<String>) {
